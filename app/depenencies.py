89613467d4e60c7e63face5b61c8329f78781c74
bkl-data-engineering <bhasker.lakshmikanth+portfolio@gmail.com>
2026-04-05 15:15:05 -0400

    Create depenencies.py

---
+++ app/depenencies.py
@@ -0,0 +1,11 @@
+from app.config import settings
+from app.clients.databricks_client import DatabricksClient
+from app.clients.llm_client import LLMClient
+
+
+def get_databricks_client() -> DatabricksClient:
+    return DatabricksClient(settings)
+
+
+def get_llm_client() -> LLMClient:
+    return LLMClient(settings)

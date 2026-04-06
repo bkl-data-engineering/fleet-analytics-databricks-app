9f8d750220defe9d6db6b92f2b0553f07331e280
bkl-data-engineering <bhasker.lakshmikanth+portfolio@gmail.com>
2026-04-05 22:45:56 -0400

    Update run.py

--- run.py
+++ run.py
@@ -3,4 +3,6 @@ import uvicorn
 
 if __name__ == "__main__":
     port = int(os.environ.get("DATABRICKS_APP_PORT", "8000"))
+    print(f"[DEBUG] DATABRICKS_APP_PORT = {os.environ.get('DATABRICKS_APP_PORT')}")
+    print(f"[DEBUG] Using port = {port}")
     uvicorn.run("app.main:app", host="0.0.0.0", port=port)

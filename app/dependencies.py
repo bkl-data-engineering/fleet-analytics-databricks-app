b7c8b41efbba4ba9220ccaf00382143481479f6c
bkl-data-engineering <bhasker.lakshmikanth+portfolio@gmail.com>
2026-04-06 15:31:37 -0400

    Update dependencies.py
    no longer using databricksclient as we changed to use the WorkspaceClient

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,10 +1,10 @@
-from app.clients.databricks_client import DatabricksClient
+#from app.clients.databricks_client import DatabricksClient
 from app.clients.llm_client import LLMClient
 from app.config import settings
 
 
-def get_databricks_client() -> DatabricksClient:
-    return DatabricksClient(settings)
+# def get_databricks_client() -> DatabricksClient:
+#    return DatabricksClient(settings)
 
 
 def get_llm_client() -> LLMClient:
